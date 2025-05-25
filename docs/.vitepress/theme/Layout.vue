cb47e9b5710a88dd6d4f72e4c22b3f65f108100f
Chemaclass <chemaclass@outlook.es>
2025-05-25 22:30:49 +0200

    add Website in progress banner on top

---
+++ docs/.vitepress/theme/Layout.vue
@@ -0,0 +1,8 @@
+<script setup>
+import DefaultTheme from 'vitepress/theme'
+</script>
+
+<template>
+  <div class="site-banner">Website under construction</div>
+  <DefaultTheme.Layout />
+</template>
